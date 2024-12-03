09201778b242bd36cd9536d6dc4fb65aec0394c6
Yashver1 <yashvershori@gmail.com>
2024-12-03 21:35:20 +0800

    removed days

--- day-2/part1/include/part1.h
+++
@@ -1,9 +0,0 @@
-#ifndef PART1_H
-#define PART1_H
-#include<string>
-
-std::string process(std::string &input);
-
-
-
-#endif // PART1
--- day-2/part2/include/part2.h
+++
@@ -1,8 +0,0 @@
-#ifndef PART2_H
-#define PART2_H
-#include<string>
-
-std::string process(std::string &input);
-
-
-#endif // PART2_H
